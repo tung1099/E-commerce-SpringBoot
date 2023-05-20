95ae0a3b9d2df4afba57269989c6a9d58192fdca
tung1099 <dtroyal1102@gmail.com>
2023-05-20 12:51:20 +0700

    login/register function

---
+++ Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
@@ -0,0 +1,83 @@
+package com.ecommerce.admin.controller;
+
+import com.ecommerce.library.dto.AdminDto;
+import com.ecommerce.library.model.Admin;
+import com.ecommerce.library.service.impl.AdminServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @GetMapping("/login")
+    public String loginForm(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("adminDto", new AdminDto());
+        return "register";
+    }
+    @GetMapping("forgot-password")
+    public String forgotPassword(){
+        return "forgot-password";
+    }
+    @PostMapping("/register-new")
+    public String addNewAdmin(@Valid @ModelAttribute("adminDto")AdminDto adminDto,
+                              BindingResult result,
+                              Model model,
+                              HttpSession session){
+
+        try {
+            session.removeAttribute("message");
+            if(result.hasErrors()){
+                model.addAttribute("adminDto", adminDto);
+                result.toString();
+                return "register";
+            }
+            String username = adminDto.getUsername();
+            Admin admin = adminService.findByUsername(username);
+            if(admin != null){
+                model.addAttribute("adminDto", adminDto);
+//                System.out.println("admin not null");
+//                model.addAttribute("emailError", "Your email has been registered!");
+                System.out.println("Admin not null");
+                session.setAttribute("message", "Your email has been registered!");
+                return "register";
+            }
+            if(adminDto.getPassword().equals(adminDto.getRepeatPassword())){
+                adminDto.setPassword(passwordEncoder.encode(adminDto.getPassword()));
+                adminService.save(adminDto);
+                System.out.println("success");
+                session.setAttribute("message", "Register successfully!");
+//                model.addAttribute("success", "Register successfully!");
+                model.addAttribute("adminDto", adminDto);
+            }else{
+                model.addAttribute("adminDto", adminDto);
+//                model.addAttribute("passwordError", "Your password maybe wrong! Check again!");
+                session.setAttribute("message", "Password is not same");
+                System.out.println("password not same");
+                return "register";
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            model.addAttribute("errors", "The server has been wrong!");
+        }
+        return "register";
+
+    }
+}
