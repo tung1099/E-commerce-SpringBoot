f396991df23e726cdaa4c887592621e3b0f18338
tung1099 <dtroyal1102@gmail.com>
2023-05-31 19:37:24 +0700

    add Customer module

---
+++ Library/src/main/java/com/ecommerce/library/dto/CustomerDto.java
@@ -0,0 +1,4 @@
+package com.ecommerce.library.dto;
+
+public class CustomerDto {
+}
--- Library/src/main/java/com/ecommerce/library/repository/ProductRepository.java
+++ Library/src/main/java/com/ecommerce/library/repository/ProductRepository.java
@@ -1,9 +1,21 @@
 package com.ecommerce.library.repository;
 
+import com.ecommerce.library.dto.ProductDto;
 import com.ecommerce.library.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("select p from Product p")
+    Page<ProductDto> pageProduct(Pageable pageable);
+    @Query("select p from Product p where p.description like %?1% or p.name like %?1%")
+    Page<ProductDto> searchProducts(String keyword, Pageable pageable);
+    @Query("select p from Product p where p.description like %?1% or p.name like %?1%")
+    List<Product> searchProductsList(String keyword);
 }
